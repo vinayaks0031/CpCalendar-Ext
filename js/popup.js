17c03ab48bb71e71cb125c0c7e4de5c83eb58b17
vinayak <vinayaks0031@gmail.com>
2021-11-28 12:46:22 +0530

    phase 1 completed:fetch&show

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,13 @@
 showData();
+
 async function getContestData() {
-    const response = await fetch("../html/data.json");
-    const data = response.json();
-    return data;
+  const response = await fetch("../html/data.json");
+  const data =await response.json();
+  return data;
 }
-function showData() {
-    let loader = `<h1 style="text-shadow: 3px 5px 5px #4B4B4C;color:white;">LeetCode</h1>
-    <div class="container min-vh-100 d-flex align-items-center justify-content-center flex-column">
+
+async function showData() {
+  let loader = `  <div class="container min-vh-100 d-flex align-items-center justify-content-center flex-column">
     <div>
       <div class="spinner-grow text-white mx-2" role="status"></div>
       <div class="spinner-grow text-primary" role="status"></div>
@@ -18,5 +19,46 @@ function showData() {
       </h6>
     </div>
   </div>`
-    document.getElementById("contest-data").innerHTML=loader;
+  document.getElementById("contest-data").innerHTML = loader;
+  let cards = "";
+  const a = await getContestData();
+  let contestData = a.contests;
+  contestData.forEach((e) => {
+    cards += `<div data-key=${e.platform} class="event_card p-2 m-md-2 mx-3 my-3 rounded" style="background-color: ${e.hex_color} ; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;font-size:14px;">
+          <div class="d-flex align-items-center justify-content-center row">
+          <div class="col-2">
+          <img src=${e.platform === "Codechef" ? "../images/codechef.svg" : e.platform === "Leetcode" ? "../images/leetcode.svg" : "../images/codeforces.png"} class="style-icon" alt="event" />
+          </div>
+          <div class=${e.platform === "Leetcode" ? "col-10" : "col-10 text-light"} style=${e.platform === "Leetcode" ? "color: #8B0000" : "color:white;"}>
+          <div class="d-flex flex-column">
+          <div class="text-center style-titleBox" >
+          <span class="style-title">${e.title}</span>
+          </div>
+          <div class="row">
+          <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+              <span style= "margin: 4px;" ><u>Start</u></span>
+              <span>${e.start}</span>
+              <span class="style-time">${e.start_time}</span>
+          </div>
+          <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+              <span style="margin: 4px;"><u>End</u></span>
+              <span>${e.end}</span>
+              <span class="style-time">${e.end_time}</span>
+          </div>
+          </div>
+          </div>
+          </div>
+          <div class="col-10 d-flex flex-column align-items-center justify-content-center" style=${e.platform === "Leetcode" ? `color: #8B0000;` : `color: white;`}>
+
+          <a href="${e.link}" style="text-decoration:none; color:white;" target="_blank">
+          <button class="btn btn-danger m-2 style-Button" >
+          Contest page &#x3e;
+          </button>
+          </a>
+
+          </div>
+          </div>
+          </div>`;
+  });
+  document.getElementById("contest-data").innerHTML = cards;
 }
